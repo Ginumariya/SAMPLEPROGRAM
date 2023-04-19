a762d5614f7c69953b0ad0f0f71aaafb26dc780c
Ginumariya <ginumariyaraju1111@gmail.com>
2023-04-19 22:46:14 +0530

    commit message

--- JENKINS/src/main/java/Jenkins.java
+++ JENKINS/src/main/java/Jenkins.java
@@ -3,5 +3,8 @@ public class Jenkins {
 public static void main(String args[]) {
 	System.out.println("JENKINS CHANGES FOR GIT HUB");
 	System.out.println("Testing");
+
+	System.out.println("changes");
+	
 }
 }
