49d4b47fb825986088745a7897b7ba08f346ec48
Ginumariya <ginumariyaraju1111@gmail.com>
2023-05-03 09:45:33 +0530

    COMMIT

--- JENKINS/src/main/java/jenkinssample/Samplepgm.java
+++ JENKINS/src/main/java/jenkinssample/Samplepgm.java
@@ -1,6 +1,6 @@
 package jenkinssample;
 
-public class Jenkins {
+public class Samplepgm {
 public static void main(String args[]) {
 	System.out.println("JENKINS CHANGES FOR GIT HUB");
 	System.out.println("Testing");
